f24971645537d5276a94d4c15e128181826c1004
AngeloDamante <angelo.damante16@gmail.com>
2023-07-04 18:32:38 +0000

    docs: add info in init file

---
+++ src/trylo_vision/scripts/__init__.py
@@ -0,0 +1,7 @@
+"""Package to handle Vision part of trilobot by Pimoroni, renamed Trylo in this context"""
+
+
+__author__ = "Angelone"
+__status__ = "Work in progress"
+__version__ = "1.0.0"
+__email__ = "angelo.damante16@gmail.com"
