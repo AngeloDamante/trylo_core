229729c4cefa02fe7811c1958467f309af0eae74
AngeloDamante <angelo.damante16@gmail.com>
2023-07-04 18:31:20 +0000

    feat: add node to handle camera

--- src/trylo_vision/scripts/n_camera.py
+++ src/trylo_vision/scripts/n_camera.py
@@ -1,1 +1,38 @@
 #!/usr/bin/env python
+
+import cv2
+import rclpy
+from rclpy.node import Node
+from cv_bridge import CvBridge
+from sensor_msgs.msg import Image
+
+# create a webcam
+webcam = cv2.VideoCapture("/dev/video0")
+
+class Camera(Node):
+    def __init__(self, fps=30):
+        super().__init__("n_camera")
+        self.publisher = self.create_publisher(Image, "/vision/camera", 5)
+        self.bridge = CvBridge()
+        self.fps = fps
+        self.timer = self.create_timer(1/self.fps, self.callback_imager)
+        
+    def callback_imager(self):
+        flag, img = webcam.read()
+        if flag:
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+            msg = self.bridge.cv2_to_imgmsg(img, encoding="passthrough")
+            self.publisher.publish(msg)
+        else:
+            self.get_logger().info("Error")
+            
+    
+def main(args=None):
+    rclpy.init(args=args)
+    node = Camera()
+    rclpy.spin(node)
+    rclpy.shutdown()
+    webcam.release()
+
+if __name__ == "__main__":
+    main()
