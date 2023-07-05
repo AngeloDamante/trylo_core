20d7bf63c2f612cf8173b845da2347619c7b49be
AngeloDamante <angelo.damante16@gmail.com>
2023-07-05 17:53:26 +0000

    docs: add info

---
+++ src/trylo_vision/trylo_vision/__init__.py
@@ -0,0 +1,7 @@
+"""Package to handle Vision part of trilobot by Pimoroni, renamed Trylo in this context"""
+
+
+__author__ = "Angelone"
+__status__ = "Tested"
+__version__ = "1.0.0"
+__email__ = "angelo.damante16@gmail.com"
