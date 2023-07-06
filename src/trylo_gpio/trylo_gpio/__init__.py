fe5509d9d657139b350ffa6cf6cea492a6c4fe22
AngeloDamante <angelo.damante16@gmail.com>
2023-07-06 09:35:26 +0000

    feat: add message type

--- src/trylo_gpio/trylo_gpio/__init__.py
+++ src/trylo_gpio/trylo_gpio/__init__.py
@@ -1,2 +1,7 @@
-""" Trylo Class 
-"""
+""" Package to handle gpio with Trylo interface """
+
+
+__author__ = "Angelone"
+__status__ = "Tested"
+__version__ = "1.0.0"
+__email__ = "angelo.damante16@gmail.com"
