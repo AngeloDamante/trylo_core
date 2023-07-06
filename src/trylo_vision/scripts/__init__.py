bfa5838fe6a1f27075d52dc54b01085efe2de00e
AngeloDamante <angelo.damante16@gmail.com>
2023-07-06 09:42:23 +0000

    docs: add comment

--- src/trylo_vision/scripts/__init__.py
+++ src/trylo_vision/scripts/__init__.py
@@ -3,6 +3,7 @@
 """
     - n_camera: Sends image taken from Pi Camera
     - n_aruco_detector: Get image and sends aruco ids and corners
+    - n_aruco_drawer: Sends image with detected corners
 """
 
 __author__ = "Angelone"
