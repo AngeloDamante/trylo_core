f4eebeeeb6016cb22e7caf3462e65d18dab70ae5
AngeloDamante <angelo.damante16@gmail.com>
2023-07-06 09:37:24 +0000

    restyle: clean code

---
+++ src/trylo_gpio/scripts/__init__.py
@@ -0,0 +1,9 @@
+"""Nodes of Gpio Package"""
+
+"""
+    - n_robot: handle motors and distance sensor on Trylo
+    - n_pub_cmd: toy node to test n_robot
+"""
+
+__author__ = "Angelone"
+__email__ = "angelo.damante16@gmail.com"
