ccf53d8e1cb753e30fc9df22cd1199fc5cc27986
AngeloDamante <angelo.damante16@gmail.com>
2023-07-16 16:30:24 +0000

    fix: add building phase and improve start scripts

--- power_on.py
+++ power_on.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 
 import os
+import time
+import argparse
 from src.trylo_gpio.trylo_gpio.Trylo import Trylo
 from src.trylo_gpio.trylo_gpio.names import (
     LIGHT_FRONT_LEFT,
@@ -10,19 +12,32 @@ from src.trylo_gpio.trylo_gpio.names import (
 )
 
 robot = Trylo()
+parser = argparse.ArgumentParser()
+parser.add_argument("-B", "--build", type=str, default="yes")
+args = parser.parse_args()
+
+build_flag = args.build
+if build_flag == "yes": os.system("make clean")
 
 for i in range(4):
     if i == 0:
         robot.set_underlight(LIGHT_MIDDLE_LEFT, (204, 0, 204))
-        os.system("make build_gpio")
+        if build_flag == "yes": os.system("make build_gpio")
+        time.sleep(0.3)
+        
     elif i == 1:
         robot.set_underlight(LIGHT_FRONT_LEFT, (102, 0, 204))
-        os.system("make build_vision")
+        if build_flag == "yes": os.system("make build_vision")
+        time.sleep(0.3)
+        
     elif i == 2:
         robot.set_underlight(LIGHT_FRONT_RIGHT, (0, 102, 204))
-        os.system("make build_control")
+        if build_flag == "yes": os.system("make build_control")
+        time.sleep(0.3)
+        
     elif i == 3:
         robot.set_underlight(LIGHT_MIDDLE_RIGHT, (0, 204, 204))
-        os.system("make build_launch")
+        if build_flag == "yes": os.system("make build_launch")
+        time.sleep(0.3)
         
 del robot
--- start_vision_control_system.sh
+++ start_vision_control_system.sh
@@ -2,13 +2,6 @@
 RED='\033[0;31m'
 NC='\033[0m' 
 
-echo "[ BUILDING ]"
-source ~/trylo_venv/bin/activate 
-cd ~/trylo_core/
-make clean
-python3 power_on.py
-echo "[ DONE ]"
-
 echo -e "${RED}---------------------------------------------------------------${NC}"
 echo -e "${RED}        _     _                               _             _  ${NC}"
 echo -e "${RED} __   _(_)___(_) ___  _ __     ___ ___  _ __ | |_ _ __ ___ | | ${NC}"
@@ -16,6 +9,12 @@ echo -e "${RED} \ \ / / / __| |/ _ \| '_ \   / __/ _ \| '_ \| __| '__/ _ \| | ${
 echo -e "${RED}  \ V /| \__ \ | (_) | | | | | (_| (_) | | | | |_| | | (_) | | ${NC}"
 echo -e "${RED}   \_/ |_|___/_|\___/|_| |_|  \___\___/|_| |_|\__|_|  \___/|_| ${NC}"
 echo -e "${RED}---------------------------------------------------------------${NC}"
+
+echo "[ BUILDING ]"
+source ~/trylo_venv/bin/activate 
+cd ~/trylo_core/
+python3 power_on.py --build "no"
+echo "[ DONE ]"
 
 cd ~/trylo_core
 source install/setup.bash
